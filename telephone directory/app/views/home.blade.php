5f642c26725d2a116c5507579def53732346ed1b
ashttley <huyenthaoo0377@gmail.com>
2024-03-11 11:13:46 +0700

    add user.index

--- telephone directory/app/views/home.blade.php
+++ telephone directory/app/views/home.blade.php
@@ -6,13 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-
-
     <link rel="stylesheet" href="/app/public/assets/home.css">
     <title>Home</title>
-    <style>
-
-    </style>
 </head>
 
 <body>
@@ -191,6 +186,7 @@
                 }
                 ?>
             </div>
+            <!--profile card -->
             <div class="profileEmployee">
                 <!-- MANAGER -->
                 <div class="card Manager">
